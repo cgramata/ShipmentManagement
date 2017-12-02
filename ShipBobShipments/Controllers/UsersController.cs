793f07e981de4f39c0b12f39ea7ea78908a21bf3
cgramata <cgrama85@gmail.com>
2017-12-02 01:56:45 -0600

    Cleans up some of the code

--- ShipBobShipments/Controllers/UsersController.cs
+++ ShipBobShipments/Controllers/UsersController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using ShipBobShipments.DAL;
 using ShipBobShipments.Models;
-using Newtonsoft.Json;
-using System.Web.Script.Serialization;
 
 namespace ShipBobShipments.Controllers
 {
